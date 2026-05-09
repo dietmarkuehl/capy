326e496da94c08856add0155660667aad7bfb650
Dietmar Kühl <dietmar.kuehl@me.com>
2026-05-09 17:14:56 -0600

    improved sndr_any_read_stream

--- bench/beman/sndr_any_read_stream.hpp
+++ bench/beman/sndr_any_read_stream.hpp
@@ -14,6 +14,7 @@
 
 #include <boost/capy/buffers.hpp>
 
+#include <memory>
 #include <utility>
 
 /// Standalone value-type erased sender stream.
@@ -24,44 +25,110 @@
 /// mechanism.
 class sndr_any_read_stream
 {
-    using read_some_fn = sndr_any_read_sender(*)(
-        void*, boost::capy::mutable_buffer);
-    using destroy_fn = void(*)(void*) noexcept;
+    struct read_some_sender {
+        using sender_concept = ex::sender_t;
+        using completion_signatures = 
+            ex::completion_signatures<ex::set_value_t(std::size_t)>;
+        template <typename...>
+        static consteval auto get_completion_signatures() {
+            return completion_signatures();
+        }
 
-    void* stream_;
-    read_some_fn read_some_;
-    destroy_fn destroy_;
+        struct inner_state_base {
+            virtual ~inner_state_base() = default;
+            virtual void start() = 0;
+            void (*set_value)(void*, std::size_t) = nullptr;
+            void* object = nullptr;
+            void complete(std::size_t size) {
+                set_value(object, size);
+            }
+        };
+
+        struct inner_receiver {
+            using receiver_concept = ex::receiver_t;
+            inner_state_base* state;
+            void set_value(std::size_t size) && noexcept {
+                state->complete(size);
+            }
+        };
+
+        template <ex::sender Sndr>
+        struct inner_state_t: inner_state_base {
+            using state_t = ex::connect_result_t<Sndr, inner_receiver>;
+            state_t state;
+            inner_state_t(Sndr&& sndr)
+                : state(ex::connect(std::forward<Sndr>(sndr), inner_receiver(this)))
+            {
+            }
+            void start() override {
+                ex::start(this->state);
+            }
+        };
+
+        template <ex::receiver Rcvr>
+        struct state {
+            using operation_state_concept = ex::operation_state_t;
+            using receiver_t = std::remove_cvref_t<Rcvr>;
+            receiver_t rcvr;
+            inner_state_base* inner_state;
+            
+            static void set_value(void* obj, std::size_t size) {
+                ex::set_value(std::move(*static_cast<receiver_t*>(obj)), size);
+            }
+            void start() & noexcept {
+                inner_state->set_value = &set_value;
+                inner_state->object = &rcvr;
+                inner_state->start();
+            }
+        };
+
+        inner_state_base* inner_state;
+
+        explicit read_some_sender(inner_state_base* st)
+            : inner_state(st)
+        {
+            static_assert(ex::sender<read_some_sender>);
+        }
+        template <ex::receiver Rcvr>
+        auto connect(Rcvr&& rcvr) && {
+            static_assert(ex::operation_state<state<Rcvr>>);
+            return state<Rcvr>(std::forward<Rcvr>(rcvr), inner_state);
+        }
+        template <typename Sndr>
+        using rep_t = std::optional<read_some_sender::inner_state_t<Sndr>>;
+    };
+    struct stream_holder_base {
+        virtual ~stream_holder_base() = default;
+        virtual read_some_sender read_some(boost::capy::mutable_buffer) = 0;
+    };
+
+    std::unique_ptr<stream_holder_base> stream_;
+
+    template <typename Stream>
+    struct stream_holder: stream_holder_base {
+        using sender_t = decltype(std::declval<Stream>().read_some(std::declval<boost::capy::mutable_buffer>()));
+
+        Stream                            stream;
+        read_some_sender::rep_t<sender_t> rep;
+        stream_holder(Stream s)
+            : stream(std::move(s))
+        {
+        }
+        read_some_sender read_some(boost::capy::mutable_buffer buf) override {
+            rep.emplace(stream.read_some(buf));
+            return read_some_sender(&*rep);
+        }
+    };
 
 public:
     template <class Stream>
     explicit sndr_any_read_stream(Stream s)
+        : stream_(new stream_holder<Stream>(std::move(s)))
     {
-        stream_ = new Stream(std::move(s));
-
-        read_some_ = +[](void* stor,
-            boost::capy::mutable_buffer buf)
-            -> sndr_any_read_sender
-        {
-            auto& stream = *static_cast<Stream*>(stor);
-            return sndr_any_read_sender{stream.read_some(buf)};
-        };
-
-        destroy_ = +[](void* stor) noexcept {
-            delete static_cast<Stream*>(stor);
-        };
     }
-
-    ~sndr_any_read_stream() { destroy_(stream_); }
-
-    sndr_any_read_stream(sndr_any_read_stream const&) = delete;
-    sndr_any_read_stream& operator=(sndr_any_read_stream const&) = delete;
-    sndr_any_read_stream(sndr_any_read_stream&&) = delete;
-    sndr_any_read_stream& operator=(sndr_any_read_stream&&) = delete;
-
-    sndr_any_read_sender
-        read_some(boost::capy::mutable_buffer buf)
+    read_some_sender read_some(boost::capy::mutable_buffer buf)
     {
-        return read_some_(stream_, buf);
+        return stream_->read_some(buf);
     }
 };
 
